28647aadac48568a46dacb2d961cd6e0bebcd058
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2019-12-13 23:36:57 +0200

    26 (About Single File Components)

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcome-message">
+    <h1>Welcome To Vue Course</h1>
+    <p>We Are</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomeMessage"
+};
+</script>
+
+<style scoped lang="scss">
+$fontSize:50px;
+h1{
+  font-size: $fontSize;
+}
+</style>
